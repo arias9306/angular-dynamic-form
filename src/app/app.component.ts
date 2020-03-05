90e390661b5a8d56dc5b0886eaaeb7b356bb3520
Andrés Arias <arias9306@gmail.com>
2020-03-05 16:10:11 -0500

    add data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,41 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
-import { InputBase } from './models/input-base';
-import { InputTextbox } from './inputs/input-textbox';
+import { Component, OnInit } from "@angular/core";
+import { Observable, of } from "rxjs";
+import { InputTextbox } from "./inputs/input-textbox";
+import { InputBase } from "./models/input-base";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
-  title = 'angular-dynamic-forms';
+  title = "angular-dynamic-forms";
   inputElements$: Observable<InputBase<any>[]>;
 
-  ngOnInit(): void {
-
-  }
+  ngOnInit(): void {}
 
   constructor() {
-
     let inputs: InputBase<string>[] = [
-       new  InputTextbox({
-          key: 'input1',
-          label: 'input 1',
-          value: '',
-          order: 1
-       })
+      new InputTextbox({
+        key: "input1",
+        label: "input 1",
+        value: "",
+        order: 1
+      }),
+      new InputTextbox({
+        key: "input2",
+        label: "input 2",
+        value: "",
+        order: 2
+      }),
+      new InputTextbox({
+        key: "input3",
+        label: "input 3",
+        value: "",
+        order: 3
+      })
     ];
 
     this.inputElements$ = of(inputs.sort((a, b) => a.order - b.order));
-
   }
-
 }
